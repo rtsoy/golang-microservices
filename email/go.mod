e96d3f29f7b43032a735ae207381252063605797
rtsoy <tsoiroman98@gmail.com>
2023-08-16 14:24:32 +0600

    feat(email): implement Kafka consumer

--- email/go.mod
+++ email/go.mod
@@ -1,3 +1,5 @@
 module email
 
 go 1.21rc4
+
+require github.com/confluentinc/confluent-kafka-go/v2 v2.2.0
--- email/main.go
+++ email/main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
 
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+const kafkaAddr = "broker:9092"
+
+func main() {
+	fmt.Println("Consumer Started")
+
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": kafkaAddr,
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	c.SubscribeTopics([]string{"default"}, nil)
+
+	// A signal handler or similar could be used to set this to false to break the loop.
+	run := true
+
+	for run {
+		msg, err := c.ReadMessage(time.Second)
+		if err == nil {
+			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		} else if !err.(kafka.Error).IsTimeout() {
+			// The client will automatically try to recover from all errors.
+			// Timeout is not considered an error because it is raised by
+			// ReadMessage in absence of messages.
+			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
+		}
+	}
+
+	c.Close()
 }
