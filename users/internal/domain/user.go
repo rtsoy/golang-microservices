a72ce0961dfc5bf7834db050e40f191552d7a52a
rtsoy <tsoiroman98@gmail.com>
2023-08-17 09:54:19 +0600

    feat(users): add User domain struct

---
+++ users/internal/domain/user.go
@@ -0,0 +1,21 @@
+package domain
+
+import "golang.org/x/crypto/bcrypt"
+
+type User struct {
+	ID           uint   `json:"id"`
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
+	Email        string `json:"email" gorm:"unique"`
+	Password     []byte `json:"-"`
+	IsAmbassador bool   `json:"isAmbassador"`
+}
+
+func (u *User) HashPassword() {
+	hash, _ := bcrypt.GenerateFromPassword(u.Password, bcrypt.DefaultCost)
+	u.Password = hash
+}
+
+func (u *User) ComparePassword(pw string) bool {
+	return bcrypt.CompareHashAndPassword(u.Password, []byte(pw)) == nil
+}
